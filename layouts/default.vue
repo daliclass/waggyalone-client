f5756e9d75eca4da74afa819308173893156f439
daliclass <markhaynes456@gmail.com>
2020-04-18 16:53:27 +0200

    Login page

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <script>
+      window.fbAsyncInit = function() {
+      FB.init({
+      appId      : '570469530243872',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v6.0'
+      });
+
+      FB.AppEvents.logPageView();
+      };
+
+      (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+      FB.getLoginStatus(function(response) {
+      console.log(response);
+      });
+    </script>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="hero is-fullheight">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <p class="navbar-item">
+          <img src="~/assets/paw.png" alt="Sa coach logo" height="50">
+        </p>
+      </div>
+    </nav>
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-12-mobile is-8-tablet is-10-desktop is-10-widescreen has-text-centered">
+            <img
+              src="~/assets/paw.png"
+              height=100
+              width=100
+              alt="Larger sa coach logo"
+              />
+            <h2 class="title is-1 ">
+              Welcome to SA coach
+            </h2>
+            <p class="subtitle">Helping dog owners help there best friend overcome separation anxiety one step at a time</p>
+            <button class="button is-info">
+              Login with Facebook
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    checkLoginState () {
+      console.log('Hello World')
+    }
+  }
+}
+</script>
