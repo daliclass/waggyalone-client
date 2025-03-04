c0f5f39bd195fd3613b9813620dc672d393250ec
Your Name <markhaynes456@gmail.com>
2025-03-04 21:45:15 +0100

    addition of pngs for logos

--- static/js/google-analytics.js
+++ static/js/google-analytics.js
@@ -1,5 +1,8 @@
-window.dataLayer = window.dataLayer || []
-function gtag () { dataLayer.push(arguments) }
-gtag('js', new Date())
+var disableGA = window.location.search.indexOf('noGA=true') > -1;
 
-gtag('config', 'G-921YK5Z1EV')
+if (!disableGA) {
+    window.dataLayer = window.dataLayer || []
+    function gtag () { dataLayer.push(arguments) }
+    gtag('js', new Date())
+    gtag('config', 'G-921YK5Z1EV')
+}
